e6f6546673d471394791efea67e7250ef3e22f50
Amir1998ali <amiralm@uci.edu>
2021-08-07 16:13:02 -0700

    submit and home pages

--- lyricsgenerator-project/music/views.py
+++ lyricsgenerator-project/music/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'music/home.html')
+
+def submit(request):
+    return render(request, 'music/submit.html')
