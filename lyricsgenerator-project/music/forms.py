f4d03c7bcc6cca436a2a3bdeaba75af88b17b819
Amir1998ali <amiralm@uci.edu>
2021-08-07 18:49:58 -0700

    styling

--- lyricsgenerator-project/music/forms.py
+++ lyricsgenerator-project/music/forms.py
@@ -1,18 +1,17 @@
 from django import forms
 from .models import Music
 
-class MusicForm(forms.Form):
-    # represent the music form we wanna create
-    artist = forms.CharField(label = "Artist", max_length=100)
-    songName = forms.CharField(label = "Song Name", max_length=100)
-    lyrics = forms.CharField(label = "Lyrics", max_length=10000)
+# class MusicForm(forms.Form):
+#     # represent the music form we wanna create
+#     artist = forms.CharField(label = "Artist", max_length=100)
+#     songName = forms.CharField(label = "Song Name", max_length=100)
+#     lyrics = forms.CharField(label = "Lyrics", max_length=10000)
+
+# you can submit different files
+class MusicForm(forms.ModelForm):
 
 
-
-# class MusicForm(forms.ModelForm):
-#     class Meta:
-#         model = Music
-#         fields = ['artist', 'songName', 'lyrics']
-#         labels = {'artist' : 'Artist' , 'songName' : 'Song Name', 'lyrics' : 'Lyrics'}
-
-
+    class Meta:
+        model = Music
+        fields = ['artist', 'songName', 'lyrics']
+        labels = {'artist' : 'Artist' , 'songName' : 'Song Name', 'lyrics' : 'Lyrics'}
--- lyricsgenerator-project/music/views.py
+++ lyricsgenerator-project/music/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import MusicForm
+from .models import Music
 
 # Create your views here.
 def home(request):
@@ -7,13 +8,15 @@ def home(request):
 
 def submit(request):
     if request.method == 'POST':
+        # the image we have here!
         filled_form = MusicForm(request.POST)
         if filled_form.is_valid():
             note = "Thanks for submitting a new song! The song %s by %s is submitted!" %(filled_form.cleaned_data['songName'], 
             filled_form.cleaned_data['artist'],)
-
-            new_form = MusicForm()
-            return render(request, 'music/submit.html', {'musicform' : new_form, 'note':note})
+            filled_form = MusicForm()
+        else :
+            note = 'Music Submission has failed! try again.'
+        return render(request, 'music/submit.html', {'musicform' : filled_form, 'note':note})
     else :
         form = MusicForm()
         return render(request, 'music/submit.html', {'musicform' : form})
